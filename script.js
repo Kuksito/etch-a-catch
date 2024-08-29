527f8e0175f81a0ec1983635152a5e9a5f564bc7
karina <123593292+Kuksito@users.noreply.github.com>
2024-08-29 13:06:13 +0300

    Add display block to rainbow btn

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ btnStart.addEventListener('click', customGrid);
 
 brushBtn.addEventListener('click', ()=> {
     setColor = 'violet';
-})
+});
 
 rainbowBtn.addEventListener('click', ()=>{
     setColor = 'rainbow';
-})
+});
 
 removeBtn.addEventListener('click', ()=>{
-    setColor = 'white';
-})
+    setColor = '#7cfc2c08';
+});
 
 function createGridOfDivs(gridSize){
     for(let i = 0; i < gridSize; i++){
@@ -36,6 +36,7 @@ function createGridOfDivs(gridSize){
     };
     brushBtn.style.display = 'block';
     removeBtn.style.display = 'block';
+    rainbowBtn.style.display = 'block';
 };
 
 function customGrid(){
@@ -51,15 +52,18 @@ function customGrid(){
 
 function getColor(){
     if(setColor === 'violet') return 'violet';
-    if(setColor === 'white') return 'white';
-    if(setColor === 'rainbow') {
-        for(let i = 0; i < 10; i++){
-        let randomColor = Math.floor(Math.random() * i);
-        return randomColor;
-        }
-    }
-
+    if(setColor === '#7cfc2c08') return '#7cfc2c08';
+    if(setColor === 'rainbow') return getRandomColor();
 };
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function applyColor(){
     this.style.backgroundColor = getColor();
