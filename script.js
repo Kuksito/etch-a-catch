d1a40092c88caac26a777a2707dd552210d7b875
karina <123593292+Kuksito@users.noreply.github.com>
2024-08-29 11:25:03 +0300

    Add loop for columns, add erase and brush btns

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
 const bodyElement = document.querySelector('body');
-const mainContainer = document.querySelector('.main-container');
-const divContainer = document.createElement('div');
-divContainer.classList.add('div-container');
-mainContainer.append(divContainer);
-let num;
+const btnContainer = document.querySelector('.btn-container');
+const container = document.querySelector('.container');
+const brushBtn = document.querySelector('.brush');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const btnStart = document.querySelector('.btn-start');
+let setColor;
+const removeBtn = document.querySelector('.remove-btn');
 
-const btnStart = document.createElement('button');
-btnStart.classList.add('btn-start');
-btnStart.textContent = 'Start';
-mainContainer.append(btnStart);
-btnStart.addEventListener('click', ()=>{
-    let numberOfSquares = prompt('Enter a number from 1 o 100');
-    if(numberOfSquares > 100) {
-        numberOfSquares = prompt('Error! Enter a number from 1 o 100')
+btnStart.addEventListener('click', customGrid);
+
+brushBtn.addEventListener('click', ()=> {
+    setColor = 'violet';
+})
+
+rainbowBtn.addEventListener('click', ()=>{
+    setColor = 'rainbow';
+})
+
+removeBtn.addEventListener('click', ()=>{
+    setColor = 'white';
+})
+
+function createGridOfDivs(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.append(row);
+
+        for(let y = 0; y < gridSize; y++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.addEventListener('mouseenter', applyColor);
+            row.append(column);
+        }
     };
-    num = numberOfSquares;
-    // deleteDivs();
-    createGridOfDivs(num);
-});
-
-function createGridOfDivs(num){
-    for(let i = 0; i < (num * num); i++){
-        const div = document.createElement('div');
-        div.classList.add('div');
-        div.addEventListener('mouseenter', ()=> {
-            div.classList.toggle('div-hover')
-        });
-        // div.style.height = '8px';
-        // div.style.width = '8px';
-        let height = div.height() + num + 'px';
-        let width = div.innerWidth() + num + 'px';
-        div.style.width = width;
-        div.style.height = height;
-        divContainer.append(div);
-        if(num > 1){}
-    }
+    brushBtn.style.display = 'block';
+    removeBtn.style.display = 'block';
 };
 
-// function deleteDivs(){
-//     const divSquares = createGridOfDivs();
-//     divSquares.remove();
-// }
+function customGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    let numberOfSquares = prompt('Enter a number between 1 and 100');
+    if(numberOfSquares > 100 || numberOfSquares <= 1) {
+        numberOfSquares = prompt('Error! Enter a number between 1 and 100')
+    };
+    return createGridOfDivs(numberOfSquares);
+};
+
+function getColor(){
+    if(setColor === 'violet') return 'violet';
+    if(setColor === 'white') return 'white';
+    if(setColor === 'rainbow') {
+        for(let i = 0; i < 10; i++){
+        let randomColor = Math.floor(Math.random() * i);
+        return randomColor;
+        }
+    }
+
+};
+
+function applyColor(){
+    this.style.backgroundColor = getColor();
+}
